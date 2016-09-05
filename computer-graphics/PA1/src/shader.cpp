76cae300839cb2d984f0a7866b99393659a46c2f
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-05 10:47:43 -0700

    Update shader.cpp

--- computer-graphics/PA1/src/shader.cpp
+++ computer-graphics/PA1/src/shader.cpp
@@ -41,7 +41,8 @@ bool Shader::AddShader(GLenum ShaderType)
 
   if(ShaderType == GL_VERTEX_SHADER)
   {
-    s((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::fileString((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    s = fileString;
     
     
     /*s = "#version 330\n \
