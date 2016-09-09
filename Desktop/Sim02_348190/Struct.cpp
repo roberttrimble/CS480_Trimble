3eb6b5140a65374f722c18a58dfd29e356a8ca28
Robert Trimble <rob_trimble92@hotmail.com>
2016-09-09 10:55:26 -0700

    Second commit

--- Desktop/Sim02_348190/Struct.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Struct.cpp
-//  Sim02
-//
-//  Created by Robert Trimble on 10/27/15.
-//  Copyright (c) 2015 Robert Trimble. All rights reserved.
-//
-
-#include "Struct.h"
--- Desktop/junk/sim01_348190/Struct.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Struct.cpp
-//  Sim01
-//
-//  Created by 348190 on 10/2/15.
-//  Copyright (c) 2015. All rights reserved.
-//
-
-#include "Struct.h"
