13cdb6d1df7ea0a7b416718327098006cfc7491d
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-11 11:07:16 -0700

    Update object.cpp

--- computer-graphics/PA2/src/object.cpp
+++ computer-graphics/PA2/src/object.cpp
@@ -79,11 +79,10 @@ Object::~Object()
 
 void Object::Update(unsigned int dt, char keyboardInput)
 {
-  /*if (keyboardInput == 'a')
+  if (keyboardInput == 'a')
     angle += dt * M_PI/1000;
-  if (keyboardInput == 's')*/
+  if (keyboardInput == 's')
     angle += dt * M_PI/ 100;
-    angle = 0;
   
   //Multiply the model's rotation by the translation
   //this causes the "orbit"
