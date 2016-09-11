6be6e30a630e46c9bd3eb24344b8f5925b37afe8
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-11 10:00:47 -0700

    Update graphics.cpp

--- computer-graphics/PA2/src/graphics.cpp
+++ computer-graphics/PA2/src/graphics.cpp
@@ -107,10 +107,10 @@ bool Graphics::Initialize(int width, int height)
   return true;
 }
 
-void Graphics::Update(unsigned int dt)
+void Graphics::Update(unsigned int d, char keyboardInput)
 {
   // Update the object
-  m_cube->Update(dt);
+  m_cube->Update(dt, keyboardInput);
 }
 
 void Graphics::Render()
