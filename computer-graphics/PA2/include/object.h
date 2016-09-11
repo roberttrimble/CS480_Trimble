0915d4cdcd8c2de9fe238d695e527773663651e3
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-11 09:47:56 -0700

    Update object.h

--- computer-graphics/PA2/include/object.h
+++ computer-graphics/PA2/include/object.h
@@ -9,7 +9,7 @@ class Object
   public:
     Object();
     ~Object();
-    void Update(unsigned int dt);
+    void Update(unsigned int dt, char keyboardInput);
     void Render();
 
     glm::mat4 GetModel();
