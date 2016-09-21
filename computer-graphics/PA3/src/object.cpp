8f1954f75512420a6be31ff9891430d2880a5234
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-20 19:52:49 -0700

    Update object.cpp

--- computer-graphics/PA3/src/object.cpp
+++ computer-graphics/PA3/src/object.cpp
@@ -85,6 +85,12 @@ Object::~Object()
   Indices.clear();
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 void Object::Update(unsigned int dt, char keyboardInput, bool newInput)
 {
   //////////////////////////
@@ -253,19 +259,201 @@ void Object::Update(unsigned int dt, char keyboardInput, bool newInput)
   
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 void Object::UpdateMoon(unsigned int dt, glm::mat4 planetModel, char keyboardInput, bool newInput)
 {
-  orbitAngle += dt * M_PI/1000;
-  rotAngle += dt * M_PI/1000;
-  model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+  //////////////////////////
+  //Check for a new keyboard input
+  //////////////////////////
+  if (newInput == true)
+  {
+    switch (keyboardInput)
+    {
+      //Start or Stop moon orbit
+      /////////////////////////////
+      case '^':
+        if (orbitMoving == true)
+          orbitMoving = false;
+        else if (orbitMoving == false)
+          orbitMoving = true;
+        break;
+      
+      //Reverse moon orbit
+      ///////////////////////
+      case 'v':
+        if (orbitForward == true)
+          orbitForward = false;
+        else if (orbitForward == false)
+          orbitForward = true;
+        break;
+        
+      //Start or stop moon rotation
+      ///////////////////////
+      case '<':
+        if (rotMoving == true)
+          rotMoving = false;
+        else if (rotMoving == false)
+          rotMoving = true;
+        break;
+        
+      //Reverse moon rotation
+      ///////////////////////
+      case '>':
+        if (rotForward == true)
+          rotForward = false;
+        else if (rotForward == false)
+          rotForward = true;
+        break;
+        
+      //Stop everything, and reset to defualt
+      //////////////////////////////////////
+      case 'b': // b for left mouse button
+        if (orbitMoving == true || orbitForward == true || rotMoving == true || rotForward == true)
+        {
+          orbitMoving = false;
+          orbitForward = false;
+          rotMoving = false;
+          rotForward = false;
+        }
+        else if (orbitMoving == false && orbitForward == false && rotMoving == false && rotForward == false)
+        {
+          orbitMoving = true;
+          orbitForward = true;
+          rotMoving = true;
+          rotForward = true;
+        }
+       
+      default:
+        break;
+    }
+  }
+  
+  /////////////////////////////
+  //Move the moon accordingly
+  /////////////////////////////
+  
+  ////////////////////////////////////////
+  //If the moon is orbiting and rotating
+  ////////////////////////////////////////
+  if (orbitMoving == true && rotMoving == true)
+  {
+    //If orbit and rotation are normal
+    if (orbitForward == true && rotForward == true)
+    {
+      orbitAngle += dt * M_PI/1000;
+      rotAngle += dt * M_PI/1000;
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
           * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
-  model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+    //If orbit is reversed but rotation is normal
+    else if (orbitForward == false && rotForward == true)
+    {
+      orbitAngle -= dt * M_PI/1000;
+      rotAngle += dt * M_PI/500; // *2 to offset speed
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+    //If orbit is normal but rotation is reversed
+    else if (orbitForward == true && rotForward == false)
+    {
+      orbitAngle += dt * M_PI/1000;
+      rotAngle -= dt * M_PI/1500; 
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+    //If orbit is reversed but rotation is reveresed
+    else if (orbitForward == false && rotForward == false)
+    {
+      orbitAngle -= dt * M_PI/1000;
+      rotAngle -= dt * M_PI/1500;
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+  }
+  /////////////////////////////////////////////
+  //If the planet is orbiting but not rotating
+  /////////////////////////////////////////////
+  else if (orbitMoving == true && rotMoving == false)
+  {
+    //If orbit is normal and roate stopped
+    if (orbitForward == true)
+    {
+      orbitAngle += dt * M_PI/1000;
+      rotAngle += 0;
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+    //If orbit is reversed and roate is stopped
+    else if (orbitForward == false)
+    {
+      orbitAngle -= dt * M_PI/1000;
+      rotAngle += 0;
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+  }
+  ////////////////////////////////////////////
+  //If the planet is not orbiting but rotating
+  ////////////////////////////////////////////
+  else if (orbitMoving == false && rotMoving == true)
+  {
+    //If orbit is stopped and roate is normal
+    if (rotForward == true)
+    {
+      orbitAngle += 0;
+      rotAngle += dt * M_PI/1000;
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+    //If orbit is stopped and roate is reversed
+    else if (rotForward == false)
+    {
+      orbitAngle += 0;
+      rotAngle -= dt * M_PI/1000;
+      model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+    }
+  }
+  ////////////////////////////////////////////
+  //If the planet is not orbiting or rotating
+  ////////////////////////////////////////////
+  else if (orbitMoving == false && rotMoving == false)
+  {
+    orbitAngle += 0;
+    rotAngle += 0;
+    model = planetModel * ((glm::rotate(glm::mat4(1.0f), (orbitAngle), glm::vec3(0.0, 12.0, 0.0)) * glm::translate(glm::mat4(1.0f), glm::vec3(2.0, 0.0, 0.0)))
+          * glm::rotate(glm::mat4(1.0f), (rotAngle), glm::vec3(0.0, 12.0, 0.0)));
+      model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+  }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 glm::mat4 Object::GetModel()
 {
   return model;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Object::Render()
 {
