15b6cc0b7089d492bcdbf00144de4bccd5812ee2
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-25 12:01:54 -0700

    Update main.cpp

--- computer-graphics/PA4/src/main.cpp
+++ computer-graphics/PA4/src/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv)
     printf("Incorrect file input.\nTry ./Assignment4 <filename>");
     return 0;
   }
-    string fileInput(argv);
+  string fileInput;
+  strcpy(fileInput, argv[1]);
   
   // Start an engine and run it then cleanup after
   Engine *engine = new Engine("Tutorial Window Name", 800, 600);
