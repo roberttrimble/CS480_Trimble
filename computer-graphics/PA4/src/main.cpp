b37a9b7bb1ddd67bab419492ab73b8ecf3ca222e
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-25 12:03:18 -0700

    Update main.cpp

--- computer-graphics/PA4/src/main.cpp
+++ computer-graphics/PA4/src/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
   
   // Start an engine and run it then cleanup after
   Engine *engine = new Engine("Tutorial Window Name", 800, 600);
-  if(!engine->Initialize(fileName))
+  if(!engine->Initialize(fileInput))
   {
     printf("The engine failed to start.\n");
     delete engine;
