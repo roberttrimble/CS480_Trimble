5d1e5355b6f5be21dd3405c0068775d432ee01e3
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-25 11:41:16 -0700

    Update main.cpp

--- computer-graphics/PA4/src/main.cpp
+++ computer-graphics/PA4/src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include string
 
 #include "engine.h"
 
 
 int main(int argc, char **argv)
 {
+  // Check for correct input
+  string fileInput;
+  if (argc != 2)
+  {
+    printf("Incorrect file input.\nTry ./Assignment4 <filename>");
+    return 0;
+  }
+  else
+  {
+    fileInput = "../" + argv;
+  }
+  
   // Start an engine and run it then cleanup after
   Engine *engine = new Engine("Tutorial Window Name", 800, 600);
   if(!engine->Initialize())
@@ -14,7 +27,7 @@ int main(int argc, char **argv)
     engine = NULL;
     return 1;
   }
-  engine->Run();
+  engine->Run(fileInput);
   delete engine;
   engine = NULL;
   return 0;
