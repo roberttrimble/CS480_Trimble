cf1dd3574107930080ad6454635d13f28408ff3c
roberttrimble <rob_trimble@nevada.unr.edu>
2016-09-27 10:18:51 -0700

    Update object.cpp

--- computer-graphics/PA4/src/object.cpp
+++ computer-graphics/PA4/src/object.cpp
@@ -43,7 +43,7 @@ Object::Object(std::string fileInput)
     {
       char format1, format2;
       unsigned int face[9];
-      fscanf(file, "%i%c%c", &face[0], &format1, &format2);
+      fscanf(file, "%i%c%c ", &face[0], &format1, &format2);
       if (format1 == '/' && format2 == '/')
       {
         fscanf(file, "%i//%i %i//%i\n", &face[1], &face[9], &face[2], &face[8]);
