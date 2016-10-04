4c49650db401eda2ca8f55434f5ab43565c0a8da
roberttrimble <rob_trimble@nevada.unr.edu>
2016-10-04 08:45:08 -0700

    Update engine.h

--- computer-graphics/PA5/include/engine.h
+++ computer-graphics/PA5/include/engine.h
@@ -13,7 +13,7 @@ class Engine
     Engine(string name, int width, int height);
     Engine(string name);
     ~Engine();
-    bool Initialize();
+    bool Initialize(string fileInput);
     void Run();
     void Keyboard();
     unsigned int getDT();
