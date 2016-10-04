c31a425773672672d6bc10baab4dad0fe1271fba
roberttrimble <rob_trimble@nevada.unr.edu>
2016-10-04 08:44:04 -0700

    Update main.cpp

--- computer-graphics/PA5/src/main.cpp
+++ computer-graphics/PA5/src/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#incluse <string>
 
 #include "engine.h"
 
 
 int main(int argc, char **argv)
 {
+  // Check for correct input
+  if (argc != 2)
+  {
+    printf("Incorrect file input.\nTry ./Assignment5 <filenpath>");
+    return 0;
+  }
+  string fileInput = argv[1];
+  
+  
   // Start an engine and run it then cleanup after
   Engine *engine = new Engine("Assignment 5", 800, 600);
-  if(!engine->Initialize())
+  if(!engine->Initialize(fileInput))
   {
     printf("The engine failed to start.\n");
     delete engine;
