69e52e911b8f003f9b666399918b536582cd87e3
Zachary T Cooper <zcooper@nxnode-0.rd.unr.edu>
2016-10-10 10:50:18 -0700

    Texture Coords

--- computer-graphics/PA6/include/graphics_headers.h
+++ computer-graphics/PA6/include/graphics_headers.h
@@ -26,8 +26,9 @@ struct Vertex
 {
   glm::vec3 vertex;
   glm::vec3 color;
+  glm::vec2 TexCoords;
 
-  Vertex(glm::vec3 v, glm::vec3 c): vertex(v), color(c) {}
+  Vertex(glm::vec3 v, glm::vec3 c, glm::vec2 t): vertex(v), color(c), TexCoords(t) {}
 };
 
 #endif /* GRAPHICS_HEADERS_H */
